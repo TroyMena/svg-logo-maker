729fe80ae97e36963afbf64883719d1f1c98186f
TroyMena <troymena@gmail.com>
2024-03-24 15:47:02 -0400

    Updated questions in the server.js file; as well as updated the shapes.js and shapes.test.js files. Also, updated the README as well.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This will create an SVG file by taking in input from the user to create a logo.
 ## Sources
 
 * Bootcamp tutor, Mansi Patel
+* Bootcamp tutor, Jose Lopez
 * A study group I worked with
 
 ## Repository URL
--- server.js
+++ server.js
@@ -1,3 +1,58 @@
-const CLI = require('./lib/cli');
+// const CLI = require('./lib/cli');
 
-new CLI().run();
+// new CLI().run();
+
+const inquirer = require('inquirer');
+const { Circle, Square, Triangle } = require('./lib/shapes');
+const { writeFile } = require('fs');
+
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like to draw?',
+        choices: ['Circle', 'Square', 'Triangle']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'What color would you like to use?'
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Please enter the text to write on top of s Shape. Text must be 3 or less characters',
+        validate: function (text) {
+            if (text.length <= 3) {
+                return true;
+            }
+            return 'Please enter a text that is 3 or less characters';
+        }
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color would you like to use for text?'
+    }
+])
+.then(answers => {
+    console.log(answers);
+    let shape;
+    switch (answers.shape) {
+        case 'Circle':
+            shape = new Circle(answers.text, answers.textColor, answers.shapeColor);
+            break;
+        case 'Square':
+            shape = new Square();
+            break;
+        case 'Triangle':
+            shape = new Triangle();
+            break;
+    }
+    
+    writeFile('logo.svg', shape.render(), (err, result)=> {
+        if (err) throw  err;
+        console.log('SVG created!!!');
+    })
+   
+});
